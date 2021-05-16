71279ce4738394aac58a485010bafc9122abb311
A-Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-05-16 14:23:14 +0300

    init game

---
+++ js/game.js
@@ -0,0 +1,7 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+canvas.width = 600;
+canvas.height = 800;
+let cnvWidth = canvas.width;
+let cnvHeight = canvas.height;
